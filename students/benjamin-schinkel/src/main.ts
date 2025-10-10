f62fda6ef5c576317d1fea137c9a404a95fb1eec
LeBenjos <benjamin.schinkel01@outlook.fr>
2025-10-10 11:16:50 +0200

    init: canvas

--- students/benjamin-schinkel/src/main.ts
+++ students/benjamin-schinkel/src/main.ts
@@ -1,7 +1,26 @@
 import './style.css';
 
+//#region Canvas
 const canvas = document.createElement('canvas');
+const context = canvas.getContext('2d');
 canvas.classList.add('canvas');
-document.appendChild(canvas);
+document.querySelector("body")!.appendChild(canvas);
+//#endregion
 
+//#region Resize
+const onResize = (): void => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+window.addEventListener('resize', onResize);
+onResize();
+//#endregion
 
+//#region Ticker
+const ticker = () => {
+  context?.clearRect(0, 0, canvas.width, canvas.height);
+
+  requestAnimationFrame(ticker);
+}
+ticker();
+//#endregion
