08233e38a5f581409ca255f0c35d50bf08c7dc1e
Robin Vigier / B!nro <rob.vigier@gmail.com>
2025-10-10 15:46:03 +0200

    ✨ Draw circle

--- students/robin-vigier/exo-02/src/main.js
+++ students/robin-vigier/exo-02/src/main.js
@@ -6,3 +6,42 @@ canvas.style.width = canvas.width / 2 + 'px';
 canvas.style.height = canvas.height / 2 + 'px';
 
 const ctx = canvas.getContext("2d");
+
+const data = {
+  size: 200,
+  color: "#2650e8ff",
+  resolution : 0.001,
+  weight: 2
+}
+
+function drawCircle() {
+
+  ctx.strokeStyle = data.color;
+  ctx.beginPath();
+  ctx.save()
+  ctx.translate(canvas.width / 2, canvas.height / 2)
+  ctx.lineWidth = data.weight;
+  // ctx.moveTo(0, 0);
+
+  ctx.moveTo(data.size, 0);
+
+  let a = Math.PI * data.resolution;
+
+  while(a<Math.PI * 2) {
+    const dx = Math.cos(a) * data.size
+    const dy = Math.sin(a) * data.size
+  
+    ctx.lineTo(dx, dy);
+
+    a+= Math.PI * data.resolution;
+  }
+
+
+  ctx.lineTo(data.size, 0);
+
+  ctx.stroke();
+  ctx.closePath();
+  ctx.restore()
+}
+
+drawCircle()
