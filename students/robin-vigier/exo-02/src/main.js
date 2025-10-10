1a98056150fdfadf115bf2600b0ad0993869142d
Robin Vigier / B!nro <rob.vigier@gmail.com>
2025-10-10 17:21:59 +0200

    ✨  circle with noise

--- students/robin-vigier/exo-02/src/main.js
+++ students/robin-vigier/exo-02/src/main.js
@@ -1,3 +1,6 @@
+import {Pane} from 'tweakpane';
+import SimplexNoise from '../libs/noise';
+
 const canvas = document.getElementById("canvas");
 
 canvas.width = window.innerWidth * 2;
@@ -10,38 +13,87 @@ const ctx = canvas.getContext("2d");
 const data = {
   size: 200,
   color: "#2650e8ff",
-  resolution : 0.001,
-  weight: 2
+  resolution: 80,
+  weight: 2,
+  noisePosRatio : 0.006,
+  noisePosStr: 6.0,
+
 }
 
+const noise = new SimplexNoise();
+
 function drawCircle() {
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+  const x = canvas.width / 2;
+  const y = canvas.height / 2;
 
   ctx.strokeStyle = data.color;
   ctx.beginPath();
   ctx.save()
-  ctx.translate(canvas.width / 2, canvas.height / 2)
+  ctx.translate(x, y)
   ctx.lineWidth = data.weight;
   // ctx.moveTo(0, 0);
 
-  ctx.moveTo(data.size, 0);
+  // ctx.moveTo(data.size, 0);
 
-  let a = Math.PI * data.resolution;
+  let a = 0
 
-  while(a<Math.PI * 2) {
+  let step = (Math.PI * 2) / data.resolution;
+
+  for (let i = 0; i < data.resolution; i++) {
     const dx = Math.cos(a) * data.size
     const dy = Math.sin(a) * data.size
-  
-    ctx.lineTo(dx, dy);
 
-    a+= Math.PI * data.resolution;
+    const noisePos = data.size + noise.noise2D(dx * data.noisePosRatio, dy * data.noisePosRatio) * data.noisePosStr
+    // const nx = dx + Math.cos(noisePos) * data.noisePosStr * 6
+    // const ny = dy + Math.sin(noisePos) * data.noisePosStr * 6
+    const nx = Math.cos(a) * noisePos
+    const ny = Math.sin(a) * noisePos
+
+
+    ctx.lineTo(nx, ny);
+
+    a += step;
   }
 
 
-  ctx.lineTo(data.size, 0);
+  // ctx.lineTo(data.size, 0);
 
-  ctx.stroke();
   ctx.closePath();
+  ctx.stroke();
   ctx.restore()
 }
 
 drawCircle()
+
+//
+
+const pane = new Pane();
+pane.addBinding(data, 'size', {
+  min: 20,
+  max: 500,
+  step: 1
+})
+pane.addBinding(data, 'resolution', {
+  min: 4,
+  max: 200,
+  step: 1
+})
+pane.addBinding(data, 'weight', {
+  min: 1,
+  max: 10,
+  step: 1
+})
+pane.addBinding(data, 'noisePosRatio', {
+  min: 0,
+  max: 0.1,
+  step: 0.001
+})
+pane.addBinding(data, 'noisePosStr', {
+  min: 0,
+  max: 200
+})
+
+pane.on('change', drawCircle);
