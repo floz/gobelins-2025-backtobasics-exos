40061d9e428629354e739278874fa54acd573237
Robin Vigier / B!nro <rob.vigier@gmail.com>
2025-10-10 15:16:29 +0200

    🚀 Start the mindblowing

---
+++ students/robin-vigier/exo-02/src/main.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("canvas");
+
+canvas.width = window.innerWidth * 2;
+canvas.height = window.innerHeight * 2;
+canvas.style.width = canvas.width / 2 + 'px';
+canvas.style.height = canvas.height / 2 + 'px';
+
+const ctx = canvas.getContext("2d");
